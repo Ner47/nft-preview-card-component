0090da35e7156131d0b325b44767b4f20dff18a7
Ner47 <neogil28@gmail.com>
2026-02-09 12:10:56 +0300

    chore: add base for component

---
+++ src/shared/assets/index.js
@@ -0,0 +1,5 @@
+export { default as iconClock } from './icon-clock.svg';
+export { default as iconEthereum } from './icon-ethereum.svg';
+export { default as iconViews } from './icon-view.svg';
+export { default as imageAvatar } from './image-avatar.png';
+export { default as imageEquilibrium } from './image-equilibrium.jpg';
